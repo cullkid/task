16e01b5dfffe3d79410ad8e000d5b0d9f53b18ef
unknown <Eze Anthony>
2023-03-10 22:41:55 +0000

    updated

--- task2/solution.js
+++ task2/solution.js
@@ -1,31 +1,19 @@
 const categorizeSimilarTransactions = (transactions) => {
   // add your code here
+  const categoriesMap = transactions.reduce((map, transaction) => {
+    if (transaction.category) {
+      map[transaction.id] = transaction.category;
+    }
+    return map;
+  }, {});
+
+  // enhance transactions with category information if available
+  return transactions.map((transaction) => {
+    if (transaction.id in categoriesMap) {
+      return { ...transaction, category: categoriesMap[transaction.id] };
+    }
+    return transaction;
+  });
 };
 
 module.exports = categorizeSimilarTransactions;
-
-
-
-
-
-
-
-
-
-// const categorizeSimilarTransactions = (transactions) => {
-//   const categoriesMap = transactions.reduce((map, transaction) => {
-//     if (transaction.category) {
-//       map[transaction.id] = transaction.category;
-//     }
-//     return map;
-//   }, {});
-
-//   // enhance transactions with category information if available
-//   return transactions.map((transaction) => {
-//     if (transaction.id in categoriesMap) {
-//       return { ...transaction, category: categoriesMap[transaction.id] };
-//     }
-//     return transaction;
-//   });
-// }
-// module.exports = categorizeSimilarTransactions
